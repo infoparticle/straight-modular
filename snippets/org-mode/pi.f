f3e2a0c81e5806f734a67d1a7ebc1be54b28d5f5
Gopi <gopi@win10>
2019-08-20 15:58:47 +0530

    migrated yasnippets

---
+++ snippets/org-mode/pi.f
@@ -0,0 +1,11 @@
+# -*- mode: snippet -*-
+# name: PI  Features Properties
+# key: pi.f
+# --
+:PROPERTIES:
+:PI: $1
+:Points: $3
+:Assigned: $5
+:Tags: $6
+:Date: `(format-time-string "%Y-%m-%d %a")`
+:END:

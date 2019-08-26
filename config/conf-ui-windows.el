93932a634e13e389bfac29cac9c489382af655f4
Gopi <gopi@win10>
2019-08-26 19:47:26 +0530

    conf-ui refactoring

--- config/conf-ui-windows.el
+++ config/conf-ui-windows.el
@@ -17,3 +17,23 @@
                      mode-line-misc-info)))
 (set-face-attribute 'mode-line           nil :background "gray90" :box '(:line-width 1 :color "gray80" ))
 (set-face-attribute 'mode-line-inactive  nil :background "gray90"   :box '(:line-width 1 :color "gray90" ))
+
+(defun highlight-selected-window ()
+  "Highlight selected window with a different background color."
+  (walk-windows (lambda (w)
+                  (unless (eq w (selected-window))
+                    (with-current-buffer (window-buffer w)
+                      (buffer-face-set '(:background "gray95"))))))
+  (buffer-face-set 'default))
+(add-hook 'buffer-list-update-hook 'highlight-selected-window)
+
+(global-hl-line-mode 1)
+(set-face-background hl-line-face "lavender")
+
+;; Adjust margins of all windows.
+(defun center-windows () ""
+  (walk-windows (lambda (window) (set-window-margins window 2 0)) nil 1))
+
+;; Listen to window changes.
+(add-hook 'window-configuration-change-hook 'center-windows)
+(global-visual-line-mode)

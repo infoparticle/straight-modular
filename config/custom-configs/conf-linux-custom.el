beaef2fc55758d8271c0c420610f1451a3de8919
Gopinath Sadasivam <gopinath-sadasivam@microfocus.com>
2020-12-14 14:04:01 +0530

    custom faces linux

--- config/custom-configs/conf-linux-custom.el
+++ config/custom-configs/conf-linux-custom.el
@@ -1,6 +1,8 @@
-(custom-set-variables
- '(left-margin 3))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(dired-subtree-depth-1-face ((t (:background "old lace"))))
  '(dired-subtree-depth-2-face ((t (:background "gray95"))))
  '(dired-subtree-depth-3-face ((t (:background "old lace"))))
@@ -9,7 +11,22 @@
  '(dired-subtree-depth-6-face ((t (:background "gray95"))))
  '(dired-subtree-depth-7-face ((t (:background "old lace"))))
  '(dired-subtree-depth-8-face ((t (:background "gray95"))))
- '(org-ellipsis ((t (:foreground "gray" :box nil :underline nil :overline nil :weight bold))))
- '(show-paren-match ((((class color) (background light)) (:background "linen")))))
-
+ '(line-number ((t (:inherit default :foreground "gray80"))))
+ '(org-block ((t (:extend t :background "gray95" :height 100 :family "Fira Code"))))
+ '(org-block-begin-line ((t (:foreground "slateblue" :underline nil))))
+ '(org-block-end-line ((t (:foreground "slateblue" :overline nil))))
+ '(org-document-info-keyword ((t (:foreground "slateblue"))))
+ '(org-document-title ((t (:foreground "DarkBlue" :overline nil :underline nil :weight bold :height 2.8 :family "Century"))))
+ '(org-drawer ((t (:foreground "grey50"))))
+ '(org-ellipsis ((t (:foreground "gray60" :background "white" :box nil :underline nil :overline nil :weight normal :height 80))))
+ '(org-level-1 ((t (:height 1.3 :weight bold :overline "#A7A7A7" :foreground "#3C3C3C" :background "#F0F0F0"))))
+ '(org-level-2 ((t (:height 1.0 :weight bold :overline "#123555" :foreground "#123555" :background "#E5F4FB"))))
+ '(org-level-3 ((t (:height 1.0 :weight bold :foreground "#005522" :background "#EFFFEF"))))
+ '(org-level-4 ((t (:height 1.0 :weight bold :slant normal :foreground "#EA6300"))))
+ '(org-level-5 ((t (:height 1.0 :weight bold :slant normal :foreground "#E3258D"))))
+ '(org-level-6 ((t (:height 1.0 :weight bold :slant italic :foreground "#0077CC"))))
+ '(org-level-7 ((t (:height 1.0 :weight bold :slant italic :foreground "#2EAE2C"))))
+ '(org-level-8 ((t (:height 1.0 :weight bold :slant italic :foreground "#FD8008"))))
+ '(org-meta-line ((t (:foreground "slateblue"))))
+ '(org-table ((t (:foreground "royalblue4"))))
 (put 'dired-find-alternate-file 'disabled nil)
